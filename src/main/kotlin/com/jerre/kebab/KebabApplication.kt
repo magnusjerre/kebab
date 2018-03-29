f9070f4a072fba10f533963393630e1a260ae597
Magnus Jerre <magnus.jerre@gmail.com>
2018-03-29 12:10:53 +0200

    Fjerner ubrukte html-filer og resolvers

--- src/main/kotlin/com/jerre/kebab/KebabApplication.kt
+++ src/main/kotlin/com/jerre/kebab/KebabApplication.kt
@@ -30,10 +30,7 @@ fun main(args: Array<String>) {
 @Configuration
 class MvcConfig : WebMvcConfigurer {
     override fun addViewControllers(registry: ViewControllerRegistry) {
-        registry.addViewController("/home").setViewName("home")
-        registry.addViewController("/").setViewName("home")
-        registry.addViewController("/hello").setViewName("hello")
-        //registry.addViewController("/login").setViewName("loginfile")
+        registry.addViewController("/").setViewName("index")
         registry.addViewController("/index").setViewName("index")
     }
 }
