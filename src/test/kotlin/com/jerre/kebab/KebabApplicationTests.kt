4046ead795142011ea278dbc9c8bf9b49686dc79
Magnus Jerre <magnus.jerre@gmail.com>
2018-04-14 15:24:06 +0200

    Legger til heroku-filer

--- src/test/kotlin/com/jerre/kebab/KebabApplicationTests.kt
+++ src/test/kotlin/com/jerre/kebab/KebabApplicationTests.kt
@@ -3,10 +3,12 @@ package com.jerre.kebab
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.TestPropertySource
 import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
+@TestPropertySource(locations = arrayOf("classpath:test.properties"))
 class KebabApplicationTests {
 
 	@Test
