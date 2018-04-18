9aadc3acde1f43bee704ff13e9f8df785e411add
Magnus Jerre <magnus.jerre@gmail.com>
2018-04-18 18:13:31 +0200

    Krever innlogging for endringer

--- frontend/src/dish/DishElement.tsx
+++ frontend/src/dish/DishElement.tsx
@@ -3,7 +3,7 @@ import { Shop } from "./../models";
 import { IDishElement } from "./dish-interfaces";
 
 export const DishElement : React.StatelessComponent<IDishElement> = ({dish, isLoggedIn, selectDish}) => (
-    <button className="list-element" onClick={() => {selectDish(dish.id)}}>
+    <button className="list-element" onClick={() => {if (isLoggedIn) selectDish(dish.id)}}>
         <h2>{dish.name}</h2>
         <span>{dish.priceSizes.map(ps => `${ps.size.name} - ${ps.price} kr\t`)}</span>
     </button>
